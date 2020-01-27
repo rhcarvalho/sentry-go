02c88fcd09ac126f57a4a44ef8d76ed0f3c55aef
Rodolfo Carvalho <rodolfo.carvalho@sentry.io>
2020-01-28 00:47:41 +0100

    WIP note

--- example/basic/main.go
+++ example/basic/main.go
@@ -22,6 +22,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("sentry.Init: %s", err)
 	}
+	// Maybe like dotnet Close should take no arguments and use
+	// ClientOptions.ShutdownTimeout, default 2s.
+	// https://github.com/getsentry/sentry-dotnet/blob/f6007f621d4935ca2570c0163ede944d7c86483f/src/Sentry/SentryOptions.cs#L240
 	defer sentry.Close(30 * time.Second) // set the timeout to a value appropriate for your program
 
 	resp, err := http.Get(os.Args[1])
