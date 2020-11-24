224a3b5f06576afd61657ae5413065fb62de4c2c
Rodolfo Carvalho <rodolfo.carvalho@sentry.io>
2020-11-24 14:20:40 +0100

    WIP clean up and reorganize serialization tests

--- marshal_test.go
+++ marshal_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -15,9 +16,152 @@ import (
 )
 
 var (
+	generate = flag.Bool("gen", false, "generate missing files in testdata")
+	update   = flag.Bool("update", false, "update files in testdata")
+)
+
+var (
 	goReleaseDate = time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
 	utcMinusTwo   = time.FixedZone("UTC-2", -2*60*60)
 )
+
+func TestEventMarshalJSON(t *testing.T) {
+	event := NewEvent()
+	event.Spans = []*Span{{
+		TraceID:        "d6c4f03650bd47699ec65c84352b6208",
+		SpanID:         "1cc4b26ab9094ef0",
+		ParentSpanID:   "442bd97bbe564317",
+		StartTimestamp: time.Unix(8, 0).UTC(),
+		EndTimestamp:   time.Unix(10, 0).UTC(),
+		Status:         "ok",
+	}}
+	event.StartTimestamp = time.Unix(7, 0).UTC()
+	event.Timestamp = time.Unix(14, 0).UTC()
+
+	got, err := json.Marshal(event)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Non transaction event should not have fields Spans and StartTimestamp
+	want := `{"sdk":{},"user":{},"timestamp":"1970-01-01T00:00:14Z"}`
+
+	if diff := cmp.Diff(want, string(got)); diff != "" {
+		t.Errorf("Event mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestStructSnapshots(t *testing.T) {
+	testSpan := &Span{
+		TraceID:      "d6c4f03650bd47699ec65c84352b6208",
+		SpanID:       "1cc4b26ab9094ef0",
+		ParentSpanID: "442bd97bbe564317",
+		Description:  `SELECT * FROM user WHERE "user"."id" = {id}`,
+		Op:           "db.sql",
+		Tags: map[string]string{
+			"function_name":  "get_users",
+			"status_message": "MYSQL OK",
+		},
+		StartTimestamp: time.Unix(0, 0).UTC(),
+		EndTimestamp:   time.Unix(5, 0).UTC(),
+		Status:         "ok",
+		Data: map[string]interface{}{
+			"related_ids":  []uint{12312342, 76572, 4123485},
+			"aws_instance": "ca-central-1",
+		},
+	}
+
+	testCases := []struct {
+		testName     string
+		sentryStruct interface{}
+	}{
+		{
+			testName:     "span",
+			sentryStruct: testSpan,
+		},
+		{
+			testName: "error_event",
+			sentryStruct: &Event{
+				Message:     "event message",
+				Environment: "production",
+				EventID:     EventID("0123456789abcdef"),
+				Fingerprint: []string{"abcd"},
+				Level:       LevelError,
+				Platform:    "myplatform",
+				Release:     "myrelease",
+				Sdk: SdkInfo{
+					Name:         "sentry.go",
+					Version:      "0.0.1",
+					Integrations: []string{"gin", "iris"},
+					Packages: []SdkPackage{{
+						Name:    "sentry-go",
+						Version: "0.0.1",
+					}},
+				},
+				ServerName:  "myhost",
+				Timestamp:   time.Unix(5, 0).UTC(),
+				Transaction: "mytransaction",
+				User:        User{ID: "foo"},
+				Breadcrumbs: []*Breadcrumb{{
+					Data: map[string]interface{}{
+						"data_key": "data_val",
+					},
+				}},
+				Extra: map[string]interface{}{
+					"extra_key": "extra_val",
+				},
+				Contexts: map[string]interface{}{
+					"context_key": "context_val",
+				},
+			},
+		},
+		{
+			testName: "transaction_event",
+			sentryStruct: &Event{
+				Type:           transactionType,
+				Spans:          []*Span{testSpan},
+				StartTimestamp: time.Unix(3, 0).UTC(),
+				Timestamp:      time.Unix(5, 0).UTC(),
+				Contexts: map[string]interface{}{
+					"trace": TraceContext{
+						TraceID:     "90d57511038845dcb4164a70fc3a7fdb",
+						SpanID:      "f7f3fd754a9040eb",
+						Op:          "http.GET",
+						Description: "description",
+						Status:      "ok",
+					},
+				},
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		test := test
+		t.Run(test.testName, func(t *testing.T) {
+			got, err := json.MarshalIndent(test.sentryStruct, "", "    ")
+			if err != nil {
+				t.Error(err)
+			}
+
+			golden := filepath.Join("testdata", fmt.Sprintf("%s.golden", test.testName))
+			if *update {
+				err := ioutil.WriteFile(golden, got, 0600)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			want, err := ioutil.ReadFile(golden)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Errorf("struct %s mismatch (-want +got):\n%s", test.testName, diff)
+			}
+		})
+	}
+}
 
 func TestMarshalJSON(t *testing.T) {
 	tests := []struct {
@@ -73,7 +217,7 @@ func TestErrorEventMarshalJSON(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			path := filepath.Join("testdata", "json", "event", fmt.Sprintf("%03d.json", i))
+			path := filepath.Join("event", fmt.Sprintf("%03d.json", i))
 			if *update {
 				WriteGoldenFile(t, path, buf.Bytes())
 			}
@@ -113,14 +257,14 @@ func TestTransactionEventMarshalJSON(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			path := filepath.Join("testdata", "json", "transaction", fmt.Sprintf("%03d.json", i))
+			path := filepath.Join("transaction", fmt.Sprintf("%03d.json", i))
 			if *update {
 				WriteGoldenFile(t, path, buf.Bytes())
 			}
 			got := buf.String()
 			want := ReadOrGenerateGoldenFile(t, path, buf.Bytes())
 			if diff := cmp.Diff(want, got); diff != "" {
-				t.Fatalf("JSON mismatch (-want +got):\n%s", diff)
+				t.Fatalf("MarshalJSON (-want +got):\n%s", diff)
 			}
 		})
 	}
@@ -162,14 +306,14 @@ func TestBreadcrumbMarshalJSON(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			path := filepath.Join("testdata", "json", "breadcrumb", fmt.Sprintf("%03d.json", i))
+			path := filepath.Join("breadcrumb", fmt.Sprintf("%03d.json", i))
 			if *update {
 				WriteGoldenFile(t, path, buf.Bytes())
 			}
 			got := buf.String()
 			want := ReadOrGenerateGoldenFile(t, path, buf.Bytes())
 			if diff := cmp.Diff(want, got); diff != "" {
-				t.Fatalf("JSON mismatch (-want +got):\n%s", diff)
+				t.Fatalf("MarshalJSON (-want +got):\n%s", diff)
 			}
 		})
 	}
@@ -177,6 +321,7 @@ func TestBreadcrumbMarshalJSON(t *testing.T) {
 
 func WriteGoldenFile(t *testing.T, path string, bytes []byte) {
 	t.Helper()
+	path = filepath.Join("testdata", "marshal", path)
 	err := os.MkdirAll(filepath.Dir(path), 0777)
 	if err != nil {
 		t.Fatal(err)
@@ -189,6 +334,7 @@ func WriteGoldenFile(t *testing.T, path string, bytes []byte) {
 
 func ReadOrGenerateGoldenFile(t *testing.T, path string, bytes []byte) string {
 	t.Helper()
+	path = filepath.Join("testdata", "marshal", path)
 	b, err := ioutil.ReadFile(path)
 	switch {
 	case errors.Is(err, os.ErrNotExist):
@@ -196,7 +342,7 @@ func ReadOrGenerateGoldenFile(t *testing.T, path string, bytes []byte) string {
 			WriteGoldenFile(t, path, bytes)
 			return string(bytes)
 		}
-		t.Fatalf("Missing golden file. Run `go test -args -gen` to generate it.")
+		t.Fatalf("Missing golden file %q. Run `go test -args -gen` to generate it.", path)
 	case err != nil:
 		t.Fatal(err)
 	}
